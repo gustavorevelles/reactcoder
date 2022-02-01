8f6e1a1ee05f6f75ae53ade2fb0e2641c5cb76e1
gustavorevelles <gustavorevelles@gmail.com>
2022-02-01 01:04:47 +0100

    NavBar created

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+      <nav className='NavBar'>
+          <div>
+              <img src={'./images/logo192.png'} alt='logo'/>
+          </div>
+        <button className='Option'>Boton</button>
+      </nav>
+    )
+}
+
+export default NavBar
