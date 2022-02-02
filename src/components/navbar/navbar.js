2760c8a8e3c2212fd2e63d5c2034ade44f21483a
gustavorevelles <gustavorevelles@gmail.com>
2022-02-02 23:02:50 +0100

    Bootstrap install
    Bootstrap install
    Create navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,11 +2,27 @@ import './NavBar.css'
 
 const NavBar = () => {
     return (
-      <nav className='NavBar'>
-          <div>
-              <img src={'./images/logo192.png'} alt='logo'/>
-          </div>
-        <button className='Option'>Boton</button>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+          <a class="navbar-brand" href="#">Logo</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Esquís</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tablas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Botas</a>
+                </li>
+                </ul>
+            </div>
       </nav>
     )
 }
